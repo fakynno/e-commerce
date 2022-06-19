45cd9eedf80c3ce69b9ecf5378d7818e0ebd6312
fakynno <fakynno@gmail.com>
2022-06-18 23:44:10 -0300

    Input p/ receber dados do componente pai

--- src/app/components/bookstore-app/product-list/product-item/product-item.component.ts
+++ src/app/components/bookstore-app/product-list/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Book } from './../model/book';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
+  @Input()
+  livro!: Book;
   constructor() { }
 
   ngOnInit(): void {
